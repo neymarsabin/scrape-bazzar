0ca2f0019fd29941c308bfbacaec4feb3936d8d1
Sabin Nepal <reddevil.sabin@gmail.com>
2020-04-11 19:43:45 +0545

    product - validate product attributes with test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+  validates :title, presence: true
+  validates :url, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
 end
