9a553005acc4381c6010edce0e8cc6c9fe9ec47c
Sabin Nepal <reddevil.sabin@gmail.com>
2020-04-12 16:09:36 +0545

    scraper - save attributes after scrape, but unerfined

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,23 @@
 class HomeController < ApplicationController
   def index
     @url = params[:url]
-    scraped_data = Scrape::Hamrobazzar.new(@url).get_info
-    product = Product.new(url: scraped_data.url, phone: scraped_data.phone, price: scraped_data.price)
-    if product.save
-      @product = product
-      flash[:success] = "Successfully saved flash"
-    else
-      flash[:alert] = "Could not save product"
+    if @url
+      scraped_data = Scrape::Hamrobazzar.new(@url)
+      scraped_data.get_info
+      product = Product.new(
+        url: scraped_data.url,
+        title: scraped_data.title,
+        mobile_number: scraped_data.phone,
+        price: scraped_data.price,
+        description: scraped_data.description
+      )
+      if product.save
+        @product = product
+        flash[:success] = "Successfully saved flash"
+      else
+        flash[:alert] = "Could not save product"
+      end
+      render 'index'
     end
-    render 'index'
   end
 end
--- app/models/concerns/scrape.rb
+++ app/models/concerns/scrape.rb
@@ -5,28 +5,39 @@ require 'mechanize'
 module Scrape
   extend ActiveSupport::Concern
   class Hamrobazzar
+    MATCHERS = [
+      "Price",
+      "Mobile Phone"
+    ].freeze
 
-    attr_accessor :url, :phone, :price, :description
+    attr_accessor :url, :phone, :price, :description, :title
 
     def initialize(url)
       @url = url
-      @phone = nil
-      @price = nil
-      @description = nil
     end
 
     def get_info
-      agent = Mechanize.new
-      page = agent.get(@url)
-      title = page.search("#white")
-      title.each do |el|
+      table_data = page.search("#white")
+      table_data.each do |el|
         text = el.parent&.content
         name = text.split(":")[0]&.strip
         value = text.split(":")[1]&.strip
-        next if !matchers.include?(name)
+        next if !MATCHERS.include?(name)
         @phone = value if name == "Mobile Phone"
         @price = value if name == "Price"
       end
+
+      # for title and description
+      @title = page.search(".title")[0].content.strip
+      description_text = page.search("b").select { |e| e.content.strip == "Description" }
+      @description = description_text.first.parent.parent.parent.parent.parent.parent.parent.children[1].content.strip
+    end
+
+    private
+
+    def page
+      agent = Mechanize.new
+      page = agent.get(@url)
     end
   end
 end
